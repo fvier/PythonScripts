b3474f3339ab5e6fea0f353c94f8f045c5f34cf7
Fernando Vier <fernandovier13@gmail.com>
2018-05-15 21:21:47 -0300

    Fazendo meu primeiro commit

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+print('hello world!')
+n1 = int(input("Digite o n1: "))
+n2 = int(input("Digite o n2: "))
+soma = n1 + n2
+print("Soma de ", n1, " e ", n2, " eh ", soma)
---
+++ multiplicacao.py
@@ -0,0 +1,4 @@
+num_1 = float(input("Digite um numero: "))
+num_2 = float(input("Digite um numero: "))
+mult = num_1 * num_2
+print("Multiplicacao de {0} e {1} eh: {2}".format(num_1, num_2, mult))
